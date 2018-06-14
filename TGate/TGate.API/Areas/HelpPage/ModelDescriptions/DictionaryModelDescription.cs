3833bae9484747c1e1162fdfb53c7483bb4be44e
Kay Otubu <olotubu@microsoft.com>
2018-06-13 21:31:27 -0700

    added tgate

---
+++ TGate/TGate.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TGate.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ TGate/TGate.Repository/Repository.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TGate.Repository
+{
+    public class Repository
+    {
+
+        public static JObject GetData() {
+            string data = "{id : 1, name : 'Otubu Kay'}";
+            JObject b = JObject.Parse(data);
+            return b; 
+        }
+    }
+}
